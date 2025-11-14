2dbd9965984c4be8854ee0874d88b5610a33e857
Ignatiy Polyakov <ardonignat@gmail.com>
2025-11-14 21:43:00 +0300

    Исправил функцию GetElement

--- modules/msghandlers/getcommands.go
+++ modules/msghandlers/getcommands.go
@@ -17,26 +17,20 @@ func GetElement(elementType string, params string) (msgToSend string) {
 					allSlice = append(allSlice, s.NameDesc, s.TitleDesc)
 				}
 				msgToSend = ("Список всех игр в Вашей коллекции:\n\n" + strings.Join(allSlice, "\n"))
-				return msgToSend
 			} else {
 				msgToSend = ("Вы ещё не добавили ни одной игры :(")
-				return msgToSend
 			}
 		} else {
 			displayRange, err := strconv.Atoi(params)
-			if err != nil {
-				if displayRange > 0 {
-					sliceOfEntireCategory := botmiscfunctions.RandomizeSlice(commonvariables.Games, displayRange)
-					sliceMsg := make([]string, 0, len(sliceOfEntireCategory))
-					for _, s := range sliceOfEntireCategory {
-						sliceMsg = append(sliceMsg, s.NameDesc, s.TitleDesc)
-					}
-					msgToSend = ("Список игр по указанному количеству (или меньше, если элементов в коллекции меньше указанного количества):\n\n" + strings.Join(sliceMsg, "\n"))
-					return msgToSend
-				} else {
-					msgToSend = (fmt.Sprintf("В качестве аргумента к команде должно быть число больше 0\nНапример: /%s 3", elementType))
-					return msgToSend
+			if err != nil || displayRange <= 0 { // Если есть ошибка конвертации(типа) ИЛИ число-аргумент МЕНЬШЕ ИЛИ РАВНО 0
+				msgToSend = (fmt.Sprintf("В качестве аргумента к команде должно быть число больше 0\nНапример: /%s 3", elementType))
+			} else {
+				sliceOfEntireCategory := botmiscfunctions.RandomizeSlice(commonvariables.Games, displayRange)
+				sliceMsg := make([]string, 0, len(sliceOfEntireCategory))
+				for _, s := range sliceOfEntireCategory {
+					sliceMsg = append(sliceMsg, s.NameDesc, s.TitleDesc)
 				}
+				msgToSend = ("Список игр по указанному количеству (или меньше, если элементов в коллекции меньше указанного количества):\n\n" + strings.Join(sliceMsg, "\n"))
 			}
 		}
 	}
@@ -60,5 +54,5 @@ func GetElement(elementType string, params string) (msgToSend string) {
 	// 	return msgToSend
 	// }
 	// return
-	return
+	return msgToSend
 }
