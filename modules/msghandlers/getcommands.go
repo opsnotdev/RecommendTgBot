54777d06e2d5f86ac20ea5a421d70b0ce18389d3
Ignatiy Polyakov <ardonignat@gmail.com>
2025-11-14 22:14:05 +0300

    Удалил лишнее из main.go в процессе выноса логики в функцию. Доделал getcommands.go для добавления элементов в оставшиеся категории

--- modules/msghandlers/getcommands.go
+++ modules/msghandlers/getcommands.go
@@ -35,24 +35,57 @@ func GetElement(elementType string, params string) (msgToSend string) {
 		}
 	}
 
-	// if elementType == "getBooks" {
-	// 	sliceOfEntireCategory := botmiscfunctions.RandomizeSlice(commonvariables.Books, params)
-	// 	sliceMsg := make([]string, 0, len(sliceOfEntireCategory))
-	// 	for _, s := range sliceOfEntireCategory {
-	// 		sliceMsg = append(sliceMsg, s.NameDesc, s.TitleDesc)
-	// 	}
-	// 	msgToSend = ("Список книг по указанному количеству (или меньше, если элементов меньше указанного количества):\n\n" + strings.Join(sliceMsg, "\n"))
-	// 	return msgToSend
-	// }
-	// if elementType == "getFilms" {
-	// 	sliceOfEntireCategory := botmiscfunctions.RandomizeSlice(commonvariables.Films, params)
-	// 	sliceMsg := make([]string, 0, len(sliceOfEntireCategory))
-	// 	for _, s := range sliceOfEntireCategory {
-	// 		sliceMsg = append(sliceMsg, s.NameDesc, s.TitleDesc)
-	// 	}
-	// 	msgToSend = ("Список фильмов по указанному количеству (или меньше, если элементов меньше указанного количества):\n\n" + strings.Join(sliceMsg, "\n"))
-	// 	return msgToSend
-	// }
-	// return
+	if elementType == "getBooks" {
+		if params == "" {
+			if len(commonvariables.Books) > 0 {
+				allSlice := make([]string, 0, len(commonvariables.Books))
+				for _, s := range commonvariables.Books {
+					allSlice = append(allSlice, s.NameDesc, s.TitleDesc)
+				}
+				msgToSend = ("Список всех книг в Вашей коллекции:\n\n" + strings.Join(allSlice, "\n"))
+			} else {
+				msgToSend = ("Вы ещё не добавили ни одной книги :(")
+			}
+		} else {
+			displayRange, err := strconv.Atoi(params)
+			if err != nil || displayRange <= 0 { // Если есть ошибка конвертации(типа) ИЛИ число-аргумент МЕНЬШЕ ИЛИ РАВНО 0
+				msgToSend = (fmt.Sprintf("В качестве аргумента к команде должно быть число больше 0\nНапример: /%s 3", elementType))
+			} else {
+				sliceOfEntireCategory := botmiscfunctions.RandomizeSlice(commonvariables.Books, displayRange)
+				sliceMsg := make([]string, 0, len(sliceOfEntireCategory))
+				for _, s := range sliceOfEntireCategory {
+					sliceMsg = append(sliceMsg, s.NameDesc, s.TitleDesc)
+				}
+				msgToSend = ("Список книг по указанному количеству (или меньше, если элементов в коллекции меньше указанного количества):\n\n" + strings.Join(sliceMsg, "\n"))
+			}
+		}
+	}
+
+	if elementType == "getFilms" {
+		if params == "" {
+			if len(commonvariables.Films) > 0 {
+				allSlice := make([]string, 0, len(commonvariables.Films))
+				for _, s := range commonvariables.Films {
+					allSlice = append(allSlice, s.NameDesc, s.TitleDesc)
+				}
+				msgToSend = ("Список всех фильмов в Вашей коллекции:\n\n" + strings.Join(allSlice, "\n"))
+			} else {
+				msgToSend = ("Вы ещё не добавили ни одного фильма :(")
+			}
+		} else {
+			displayRange, err := strconv.Atoi(params)
+			if err != nil || displayRange <= 0 { // Если есть ошибка конвертации(типа) ИЛИ число-аргумент МЕНЬШЕ ИЛИ РАВНО 0
+				msgToSend = (fmt.Sprintf("В качестве аргумента к команде должно быть число больше 0\nНапример: /%s 3", elementType))
+			} else {
+				sliceOfEntireCategory := botmiscfunctions.RandomizeSlice(commonvariables.Films, displayRange)
+				sliceMsg := make([]string, 0, len(sliceOfEntireCategory))
+				for _, s := range sliceOfEntireCategory {
+					sliceMsg = append(sliceMsg, s.NameDesc, s.TitleDesc)
+				}
+				msgToSend = ("Список фильмов по указанному количеству (или меньше, если элементов в коллекции меньше указанного количества):\n\n" + strings.Join(sliceMsg, "\n"))
+			}
+		}
+	}
+
 	return msgToSend
 }
